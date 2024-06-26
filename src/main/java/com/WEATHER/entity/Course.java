92dfaf420e2b89e38e57938fe4672262dc8d3515
ra1n80y <godfreyachu1@gmail.com>
2024-06-26 15:13:25 +0200

    Removed @NoArgsConstructor annotation

--- src/main/java/com/WEATHER/entity/Course.java
+++ src/main/java/com/WEATHER/entity/Course.java
@@ -3,12 +3,10 @@ package com.WEATHER.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 import org.springframework.hateoas.RepresentationModel;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
 public class Course extends RepresentationModel<Course>
 {
